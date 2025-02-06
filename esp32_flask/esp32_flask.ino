8e1d7e52cd0ca97c05092a6506a677fd2ef84398
Ahmed <AhmedBahlaoui@outlook.com>
2025-02-06 19:11:11 +0100

    added pins.h and tidying up things

--- esp32_flask/esp32_flask.ino
+++ esp32_flask/esp32_flask.ino
@@ -2,31 +2,13 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <HTTPClient.h>
-
+#include "pins.h"
 // Replace with your network credentials
 const char* ssid = "esp32_dev";
 const char* password = "12345678";
 
 // Replace with your Flask server IP and endpoint
 const char* serverUrl = "http://192.168.143.209:5000/upload";
-
-// Camera configuration for AI Thinker ESP32-CAM
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
 
 WebServer server(80);  // Create a web server on port 80
 
